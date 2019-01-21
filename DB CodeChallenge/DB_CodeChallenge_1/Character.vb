5350f3b3f1f90cf9c1e1d8441693484a4ea396c9
LEKtopia <24890955+LEKtopia@users.noreply.github.com>
2019-01-20 22:21:40 -0600

    Initial commit - add files via upload

---
+++ DB CodeChallenge/DB_CodeChallenge_1/Character.vb
@@ -0,0 +1,25 @@
+﻿Public Class Character
+
+    Private cName As String
+    Private cRace As String
+
+
+    Public Property Name() As String
+        Get
+            Return cName
+        End Get
+        Set(ByVal value As String)
+            cName = value
+        End Set
+    End Property
+
+    Public Property Race() As String
+        Get
+            Return cRace
+        End Get
+        Set(ByVal value As String)
+            cRace = value
+        End Set
+    End Property
+
+End Class
---
+++ DB CodeChallenge/DB_CodeChallenge_1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+
+    Dim lotrCharacters As New BindingList(Of Character)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
